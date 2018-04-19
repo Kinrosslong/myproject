9bcd88471dce4c1c312cc8b50d434c8d728ca595
Kinrosslong <longkinross@gmail.com>
2018-04-19 10:41:22 +0800

    设置网站头部图标

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,6 +5,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="{{ asset('home/i/favicon.png') }}">
         <!-- csrf-token 防止报错-->
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Kinross blog</title>
