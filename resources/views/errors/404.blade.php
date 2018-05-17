4dc7952e6ae0bee26c907e7aa47ec8b6e2881a42
Kinrosslong <longkinross@gmail.com>
2018-05-17 13:46:26 +0800

    添加404页面

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,7 +1,31 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Tumi
- * Date: 2018/5/17
- * Time: 11:13
- */
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Kinross 博客">
+    <meta name="keywords" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="icon" type="image/png" href="{{ asset('home/i/favicon.png') }}">
+    <title>404</title>
+    <link href="{{ asset('home/css/404.css') }}" rel="stylesheet">
+</head>
+
+<body translate="no">
+    <div class="content">
+        <canvas class="snow" id="snow" width="1349" height="400"></canvas>
+        <div class="main-text">
+            <h1>哎呀^~^页面不见了</h1>
+            <div class="main-text-a"><a href="/">返回首页</a></div>
+        </div>
+        <div class="ground">
+            <div class="mound">
+                <div class="mound_text">404 </div>
+                <div class="mound_spade"></div>
+            </div>
+        </div>
+    </div>
+</body>
+<script src="{{ asset('home/js/404.js') }}"></script>
+</html>
