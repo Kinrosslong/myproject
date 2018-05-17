d1d729e92d5abd2e8892fcf6e213da9893cce887
Kinrosslong <longkinross@gmail.com>
2018-05-17 13:39:47 +0800

    添加404页面

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tumi
+ * Date: 2018/5/17
+ * Time: 11:13
+ */
---
+++ resources/views/route.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tumi
+ * Date: 2018/5/16
+ * Time: 10:41
+ */
