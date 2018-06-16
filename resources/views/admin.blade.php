f308c40648da0220d3e07c58c9013f49a91b9cca
Kinrosslong <longkinross@gmail.com>
2018-06-16 14:47:48 +0800

    11

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,1 +1,19 @@
-<!DOCTYPE html><html><head><meta charset=utf-8><title>后台管理系统</title><meta name=viewport content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"><meta name=keywords content="vue, vue-element-system, 后台管理系统, kinross"><meta name=description content="基于Vue2 + Element UI 的后台管理系统解决方案"><link href=./static/css/app.4baff7c6f3764f7eaf0c8e00a86b9cee.css rel=stylesheet></head><body><div id=app></div><script type=text/javascript src=./static/js/manifest.8eb9069a8661a9e4a8f2.js></script><script type=text/javascript src=./static/js/vendor.d935eb6e57d9c3a3b8f4.js></script><script type=text/javascript src=./static/js/app.33e505ee95f1c0dab418.js></script></body></html>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=utf-8>
+    <title>后台管理系统</title>
+    <meta name=viewport content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
+    <meta name=keywords content="vue, vue-element-system, 后台管理系统, kinross">
+    <meta name=description content="基于Vue2 + Element UI 的后台管理系统解决方案">
+    <link href=./static/css/app.4baff7c6f3764f7eaf0c8e00a86b9cee.css rel=stylesheet>
+    <link rel="icon" type="image/png" href="{{ asset('home/i/favicon.png') }}"> 
+</head>
+<body>
+    <div id=app>
+</div>
+    <script type=text/javascript src=./static/js/manifest.8eb9069a8661a9e4a8f2.js></script>
+    <script type=text/javascript src=./static/js/vendor.d935eb6e57d9c3a3b8f4.js></script>
+    <script type=text/javascript src=./static/js/app.33e505ee95f1c0dab418.js></script>
+</body>
+</html>
