c517489a966eecfba9bdf636ad7671cb5bace54e
Kinrosslong <longkinross@gmail.com>
2018-06-29 16:04:37 +0800

    完善文章前端页面

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -8,14 +8,15 @@
     <meta name=description content="基于Vue2 + Element UI 的后台管理系统解决方案">
     <!-- csrf-token 防止报错-->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link href=./static/css/app.594a0cddb0f7fcc601bef6d14bc0c4d2.css rel=stylesheet>
+    {{--<link href=./static/css/app.594a0cddb0f7fcc601bef6d14bc0c4d2.css rel=stylesheet>--}}
+    <link href=./static/css/app.80048862cc29c45e9db6c0a612407e37.css rel=stylesheet>
     <link rel="icon" type="image/png" href="{{ asset('home/i/favicon.png') }}">
 </head>
 <body>
     <div id='app'>
     </div>
-<script type=text/javascript src=./static/js/manifest.52daf35d3c01900a3ddf.js></script>
+<script type=text/javascript src=./static/js/manifest.f3214c52a99995383688.js></script>
 <script type=text/javascript src=./static/js/vendor.d704e17647e73e432691.js></script>
-<script type=text/javascript src=./static/js/app.19ff3439c7ea139a4226.js></script>
+<script type=text/javascript src=./static/js/app.384be453fec0354e8a97.js></script>
 </body>
 </html>
--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/acticleInit', 'ArticleController@index'); //首页初始化get请求
-Route::post('/acticleDel', 'ArticleController@articleDel'); //删除
 Route::get('/acticleList', 'Admin\ArticleController@index'); //前后分离测试数据
 Route::post('/articleEdit', 'Admin\ArticleController@articleEdit'); //文章编辑
 Route::post('/articleAdd', 'Admin\ArticleController@articleAdd'); //文章编辑
+Route::post('/articleDel', 'Admin\ArticleController@articleDel'); //文章删除
+Route::post('/articleDelAll', 'Admin\ArticleController@articleDelAll'); //文章批量删除
