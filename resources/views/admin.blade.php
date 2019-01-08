28adb6ca8809c4bb9823f504d9c97e4ba30e4310
Kinrosslong <longkinross@gmail.com>
2019-01-08 11:03:10 +0800

    修改bug

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
-<html><head>
+<html>
+<head>
 <meta charset=utf-8>
-<title>后台管理系统
-</title>
+<title>后台管理系统</title>
 <meta name=viewport content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
 <meta name=keywords content="vue, vue-element-system, 后台管理系统, kinross">
 <meta name=description content="基于Vue2 + Element UI 的后台管理系统解决方案">
-<link href=./static/css/app.f1ebf21aed229a42d543bb80c28b909e.css rel=stylesheet>
+<link href="./static/css/app.517ec1dfb4e909186f12a1faa2fc650d.css" rel=stylesheet>
 </head>
 <body>
-    <div id="app"></div>
-    <script type=text/javascript src=./static/js/manifest.6b9ea6a58d06a74dfd56.js> </script>
-    <script type=text/javascript src=./static/js/vendor.d704e17647e73e432691.js></script>
-    <script type=text/javascript src=./static/js/app.34591044a2aea69d1bec.js></script>
+<div id="app">
+</div>
+<script type="text/javascript" src="./static/js/manifest.405ebfbacf50a1f3d7fb.js"></script>
+<script type="text/javascript" src="./static/js/vendor.d704e17647e73e432691.js"></script>
+<script type="text/javascript" src="./static/js/app.cdf9438c5279424cd9c5.js"></script>
 </body>
 </html>
